770a407c6c976fa894c8c9deec24e781db49c209
muralimandula <murali.mandula@msitprogram.net>
2018-11-05 18:08:36 +0530

    "ads2_m1_a1_1: 7/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ads2-assignments/m1/1.1 Graph/Solution.java
+++ ads2-assignments/m1/1.1 Graph/Solution.java
@@ -3,90 +3,88 @@ import java.util.Scanner;
  * Interface for graph.
  */
 interface Graph {
-	/**
-	 * number of vertices.
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	int vertices();
-	/**
-	 * number of edges.
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	int edges();
-	/**
-	 * Adds an edge.
-	 *
-	 * @param      v     { parameter_description }
-	 * @param      w     { parameter_description }
-	 */
-	void addEdge(int v, int w);
-	/**
-	 * iterable.
-	 *
-	 * @param      v     { parameter_description }
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	Iterable<Integer> adj(int v);
-	/**
-	 * Determines if it has edge.
-	 *
-	 * @param      v     { parameter_description }
-	 * @param      w     { parameter_description }
-	 *
-	 * @return     True if has edge, False otherwise.
-	 */
-	boolean hasEdge(int v, int w);
+    /**
+     * number of vertices.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    int vertices();
+    /**
+     * number of edges.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    int edges();
+    /**
+     * Adds an edge.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     */
+    void addEdge(int v, int w);
+    /**
+     * iterable.
+     *
+     * @param      v     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    Iterable<Integer> adj(int v);
+    /**
+     * Determines if it has edge.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     *
+     * @return     True if has edge, False otherwise.
+     */
+    boolean hasEdge(int v, int w);
 }
 /**
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		// empty constructor.
-	}
-	/**
-	 * main method.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String s = scan.nextLine();
-		int v = Integer.parseInt(scan.nextLine());
-		int e = Integer.parseInt(scan.nextLine());
-		int temp = e;
-		String[] data = scan.nextLine().split(",");
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        // empty constructor.
+    }
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        int v = Integer.parseInt(scan.nextLine());
+        int e = Integer.parseInt(scan.nextLine());
+        int temp = e;
+        String[] data = scan.nextLine().split(",");
 
-		if (s.equals("List")) {
-			//graphlist of size v.
-			GraphList list = new GraphList(v);
-			while (temp > 0) {
-				String[] fun = scan.nextLine().split(" ");
-				list.addEdge(Integer.parseInt(fun[0]),
-							 Integer.parseInt(fun[1]));
-				temp--;
-			}
-			System.out.println(list.display(data));
+        if (s.equals("List")) {
+            //graphlist of size v.
+            GraphList list = new GraphList(v);
+            while (temp > 0) {
+                String[] fun = scan.nextLine().split(" ");
+                list.addEdge(Integer.parseInt(fun[0]),
+                             Integer.parseInt(fun[1]));
+                temp--;
+            }
+            System.out.println(list.display(data));
 
-		} else if (s.equals("Matrix")) {
-			temp = e;
-			//FOr case, Graph Matrix.
-			GraphMatrix matrix = new GraphMatrix(v);
-			while (temp > 0) {
-				String[] fun = scan.nextLine().split(" ");
-				matrix.addEdge(Integer.parseInt(fun[0]),
-							Integer.parseInt(fun[1]));
-				temp--;
-			}
-			System.out.println(matrix);
-		}
-	}
+        } else if (s.equals("Matrix")) {
+            temp = e;
+            //FOr case, Graph Matrix.
+            GraphMatrix matrix = new GraphMatrix(v);
+            while (temp > 0) {
+                String[] fun = scan.nextLine().split(" ");
+                matrix.addEdge(Integer.parseInt(fun[0]),
+                            Integer.parseInt(fun[1]));
+                temp--;
+            }
+            System.out.println(matrix);
+        }
+    }
 }
-
-
