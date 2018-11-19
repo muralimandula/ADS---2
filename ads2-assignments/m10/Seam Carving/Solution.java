aaa1f41df420c2bb1158e91e420002b47be2653d
muralimandula <murali.mandula@msitprogram.net>
2018-11-19 10:34:22 +0530

    "ads2_m10_a1_1: 9/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ads2-assignments/m10/Seam Carving/Solution.java
+++ ads2-assignments/m10/Seam Carving/Solution.java
@@ -54,8 +54,8 @@ public final class Solution {
             for (int col = 0; col < carver.width(); col++) {
                 double energy = carver.energy(col, row);
                 String marker = " ";
-                if ((direction && row == seam[col])
-                 || (direction && col == seam[row])) {
+                if ((direction == true) && (row == seam[col])
+                 || (direction == false) && (col == seam[row])) {
                     marker = "*";
                     totalSeamEnergy += energy;
                 }
